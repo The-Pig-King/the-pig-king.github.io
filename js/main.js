574a103cd4459053ca4012f53ce74bc87980e983
ThePigKing <thepigkingyt@gmail.com>
2026-06-08 17:23:01 +0100

    Auto-fit nav grid, started database

--- js/main.js
+++ js/main.js
@@ -6,3 +6,12 @@ const currentYear = new Date().getFullYear();
 
 year.textContent =
   currentYear > startYear ? `${startYear} - ${currentYear}` : startYear;
+
+const slimes = [
+  {
+    name: 'Pink Slime',
+    icon: '/assets/games/slime-rancher/slimes/regular/iconSlimePink.png',
+  },
+];
+
+const cards = document.getElementById('cards');
