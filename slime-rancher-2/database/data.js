91271f1ba3413804d03197be7cd683546c8c886c
ThePigKing <thepigkingyt@gmail.com>
2026-06-20 13:47:57 +0100

    feat: added some sr2 slimes

--- slime-rancher-2/database/data.js
+++ slime-rancher-2/database/data.js
@@ -39,51 +39,134 @@ const rawCards = [
     tags: ['slime', 'common', 'docile'],
     locations: {
       spawn: [
-        'the-dry-reef',
-        'the-moss-blanket',
-        'the-indigo-quarry',
-        'the-ancient-ruins',
-        'the-glass-desert',
-        'the-slimeulation',
-        'the-slime-sea',
+        'the-rainbow-fields',
+        'starlight-strand',
+        'ember-valley',
+        'powderfall-bluffs',
       ],
-      worldgen: ['the-ranch'],
-      crate: [
-        'the-dry-reef',
-        'the-moss-blanket',
-        'the-indigo-quarry',
-        'the-ancient-ruins',
-        'the-glass-desert',
-        'the-wilds',
-        'the-slime-sea',
-      ],
-      'range-exchange': ['the-ranch'],
-      gordo: [
-        'the-dry-reef',
-        'the-moss-blanket',
-        'the-indigo-quarry',
-        'the-ancient-ruins',
-        'the-glass-desert',
-        'the-wilds',
-        'the-slime-sea',
-      ],
-      'spawn-largo': [
-        'the-dry-reef',
-        'the-moss-blanket',
-        'the-indigo-quarry',
-        'the-glass-desert',
-        'the-wilds',
-        'the-slimeulation',
-        'the-slime-sea',
-        'the-vaults',
-      ],
+      worldgen: ['the-conservatory'],
+      gordo: ['the-rainbow-fields'],
     },
     details: {
-      diet: ['veggie', 'fruit', 'meat', 'spicy-tofu'],
+      diet: ['veggie', 'fruit', 'meat'],
       'fav-toy': ['beach-ball'],
       plort: ['pink-plort'],
       largoable: ['yes'],
-      gordo: ['pink-gordo', 'party-gordo'],
+      gordo: ['pink-gordo'],
+    },
+  },
+  {
+    name: 'Cotton Slime',
+    icon: '/assets/games/slime-rancher-2/slimes/regular/iconSlimeCotton.png',
+    'radiant-icon':
+      '/assets/games/slime-rancher-2/slimes/radiant/iconRadiantSlimeCotton.png',
+    tags: ['slime', 'common', 'docile'],
+    locations: {
+      spawn: ['the-rainbow-fields', 'starlight-strand', 'powderfall-bluffs'],
+      gordo: ['the-rainbow-fields'],
+    },
+    details: {
+      diet: ['veggie'],
+      'fav-food': ['water-lettuce'],
+      'fav-toy': ['bouncy-ball'],
+      plort: ['cotton-plort'],
+      largoable: ['yes'],
+      gordo: ['cotton-gordo'],
+    },
+  },
+  {
+    name: 'Tabby Slime',
+    'secret-style-name': 'Tiger',
+    icon: '/assets/games/slime-rancher-2/slimes/regular/iconSlimeTabby.png',
+    'radiant-icon':
+      '/assets/games/slime-rancher-2/slimes/radiant/iconRadiantSlimeTabby.png',
+    'secret-style-icon':
+      '/assets/games/slime-rancher/slimes/secret-style/iconSlimeTabbyExotic.png',
+    'sr-icon': '/assets/games/slime-rancher/slimes/regular/iconSlimeTabby.png',
+    tags: ['slime', 'common', 'docile'],
+    locations: {
+      spawn: ['the-rainbow-fields', 'ember-valley'],
+      gordo: ['ember-valley'],
+    },
+    details: {
+      diet: ['meat'],
+      'fav-food': ['stony-hen'],
+      'fav-toy': ['yarn-ball'],
+      plort: ['tabby-plort'],
+      largoable: ['yes'],
+      gordo: ['tabby-gordo'],
+    },
+  },
+  {
+    name: 'Phosphor Slime',
+    'secret-style-name': 'Angelic',
+    icon: '/assets/games/slime-rancher-2/slimes/regular/iconSlimePhosphor.png',
+    'radiant-icon':
+      '/assets/games/slime-rancher-2/slimes/radiant/iconRadiantSlimePhosphor.png',
+    'secret-style-icon':
+      '/assets/games/slime-rancher/slimes/secret-style/iconSlimePhosphorExotic.png',
+    'sr-icon':
+      '/assets/games/slime-rancher/slimes/regular/iconSlimePhosphor.png',
+    tags: ['slime', 'common', 'docile'],
+    locations: {
+      spawn: [
+        'the-rainbow-fields',
+        'starlight-strand',
+        'ember-valley',
+        'powderfall-bluffs',
+      ],
+      gordo: ['the-rainbow-fields'],
+    },
+    details: {
+      diet: ['fruit'],
+      'fav-food': ['cuberry'],
+      'fav-toy': ['night-light'],
+      plort: ['phosphor-plort'],
+      largoable: ['yes'],
+      gordo: ['phosphor-gordo'],
+    },
+  },
+  {
+    name: 'Honey Slime',
+    'secret-style-name': 'Royal Jelly',
+    icon: '/assets/games/slime-rancher-2/slimes/regular/iconSlimeHoney.png',
+    'radiant-icon':
+      '/assets/games/slime-rancher-2/slimes/radiant/iconRadiantSlimeHoney.png',
+    'secret-style-icon':
+      '/assets/games/slime-rancher/slimes/secret-style/iconSlimeHoneyExotic.png',
+    'sr-icon': '/assets/games/slime-rancher/slimes/regular/iconSlimeHoney.png',
+    tags: ['slime', 'common', 'docile'],
+    locations: {
+      spawn: ['starlight-strand'],
+      gordo: ['starlight-strand'],
+    },
+    details: {
+      diet: ['fruit'],
+      'fav-food': ['mint-mango'],
+      'fav-toy': ['buzzy-bee'],
+      plort: ['honey-plort'],
+      largoable: ['yes'],
+      gordo: ['honey-gordo'],
+    },
+  },
+  {
+    name: 'Puddle Slime',
+    'secret-style-name': 'Lilypad',
+    icon: '/assets/games/slime-rancher-2/slimes/regular/iconSlimePuddle.png',
+    'radiant-icon':
+      '/assets/games/slime-rancher-2/slimes/radiant/iconRadiantSlimePuddle.png',
+    'secret-style-icon':
+      '/assets/games/slime-rancher/slimes/secret-style/iconSlimePuddleExotic.png',
+    'sr-icon': '/assets/games/slime-rancher/slimes/regular/iconSlimePuddle.png',
+    tags: ['slime', 'common', 'docile'],
+    locations: {
+      spawn: ['starlight-strand', 'ember-valley', 'powderfall-bluffs'],
+    },
+    details: {
+      diet: ['water'],
+      'fav-toy': ['rubber-ducky'],
+      plort: ['puddle-plort'],
+      largoable: ['no'],
     },
   },
 ];
